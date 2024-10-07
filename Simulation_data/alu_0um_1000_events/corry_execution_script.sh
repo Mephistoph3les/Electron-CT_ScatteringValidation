dea6e85220aea2a39bc274b65297d4a81979d5fd
Kevin Dojan <75090947+Mephistoph3les@users.noreply.github.com>
2024-10-07 15:25:29 +0200

    Add files via upload

---
+++ Simulation_data/alu_0um_1000_events/corry_execution_script.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/bash
+
+source ../../../../corryvreckan/etc/setup_lxplus.sh
+../../../../corryvreckan/bin/corry -c corry_EM_Opt1.conf
+#../../../../corryvreckan/bin/corry -c corry_EM_Opt3.conf
+#../../../../corryvreckan/bin/corry -c corry_EM_Opt4.conf
