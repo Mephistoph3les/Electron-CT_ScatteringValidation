e3a47c254c8ab7c6e50441d26fc349d2056ed228
Kevin Dojan <75090947+Mephistoph3les@users.noreply.github.com>
2024-10-07 15:28:15 +0200

    Add files via upload
    Added Simulation of 70um and 200um

---
+++ Simulation_data/alu_70um_1000_events/corry_execution_script.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+
+corry -c corry_EM_LIV.conf
+corry -c corry_EM_PEN.conf
+corry -c corry_EM_Opt0.conf
+corry -c corry_EM_Opt1.conf
+corry -c corry_EM_Opt2.conf
+corry -c corry_EM_Opt3.conf
+corry -c corry_EM_Opt4.conf
